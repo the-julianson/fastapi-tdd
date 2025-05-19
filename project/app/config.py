5908a7ce041e966e7ae8fb4b87989f8cdecfd47d
jbazanaguirre <jbazanaguirre@gmail.com>
2025-05-19 14:47:58 -0300

    Modify settings to add the database_url

--- project/app/config.py
+++ project/app/config.py
@@ -1,8 +1,10 @@
 # project/app/config.py
 
+
 import logging
 from functools import lru_cache
 
+from pydantic import AnyUrl
 from pydantic_settings import BaseSettings
 
 
@@ -12,6 +14,7 @@ log = logging.getLogger("uvicorn")
 class Settings(BaseSettings):
     environment: str = "dev"
     testing: bool = 0
+    database_url: AnyUrl = None
 
 
 @lru_cache()
