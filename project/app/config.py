fa641ce41f74df36e5970e8bbf8a7dbcf122017c
jbazanaguirre <jbazanaguirre@gmail.com>
2025-05-19 14:40:28 -0300

    Started the proyect, using Poetry, and Docker

---
+++ project/app/config.py
@@ -0,0 +1,20 @@
+# project/app/config.py
+
+import logging
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings
+
+
+log = logging.getLogger("uvicorn")
+
+
+class Settings(BaseSettings):
+    environment: str = "dev"
+    testing: bool = 0
+
+
+@lru_cache()
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from the environment...")
+    return Settings()
