1306500d2c02cefc353267841356fde3131338a3
jbazanaguirre <jbazanaguirre@gmail.com>
2025-05-19 17:32:09 -0300

    Cleaning up the main file and having Tortoise generates the schemas asynchronously

--- project/app/main.py
+++ project/app/main.py
@@ -7,22 +7,15 @@ from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
 from app.api import ping
-
+from app.db import init_db
 
 def create_application() -> FastAPI:
     application = FastAPI()
-
-    register_tortoise(
-        application,
-        db_url=os.environ.get("DATABASE_URL"),
-        modules={"models": ["app.models.tortoise"]},
-        generate_schemas=False,
-        add_exception_handlers=True,
-    )
-
     application.include_router(ping.router)
 
     return application
 
 
 app = create_application()
+
+init_db(app)
