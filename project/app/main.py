bd81e145a9234d855d46d84895b9407be51bf6dd
jbazanaguirre <jbazanaguirre@gmail.com>
2025-05-20 00:30:57 -0300

    Add Post summary and tests

--- project/app/main.py
+++ project/app/main.py
@@ -3,14 +3,16 @@
 
 from fastapi import FastAPI
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
-
+    application.include_router(
+        summaries.router, prefix="/summaries", tags=["summaries"]
+    )
     return application
 
 
---
+++ project/tests/test_summaries.py
@@ -0,0 +1,30 @@
+# flake8: noqa
+# project/tests/test_summaries.py
+
+import json
+
+import pytest
+
+
+def test_create_summary(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
+
+    assert response.status_code == 201
+    assert response.json()["url"] == "https://foo.bar"
+
+
+def test_create_summaries_invalid_json(test_app):
+    response = test_app.post("/summaries/", data=json.dumps({}))
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "input": {},
+                "loc": ["body", "url"],
+                "msg": "Field required",
+                "type": "missing",
+            }
+        ]
+    }
