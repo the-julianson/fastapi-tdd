b11e6cca9538515a421804c79426303a296b1f37
jbazanaguirre <jbazanaguirre@gmail.com>
2025-05-20 17:33:27 -0300

    Add trailing slash to expected urls

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -12,7 +12,7 @@ def test_create_summary(test_app_with_db):
     )
 
     assert response.status_code == 201
-    assert response.json()["url"] == "https://foo.bar"
+    assert response.json()["url"] == "https://foo.bar/"
 
 
 def test_create_summaries_invalid_json(test_app_with_db):
@@ -49,7 +49,7 @@ def test_read_summary(test_app_with_db):
 
     response_dict = response.json()
     assert response_dict["id"] == summary_id
-    assert response_dict["url"] == "https://foo.bar"
+    assert response_dict["url"] == "https://foo.bar/"
     assert response_dict["summary"]
     assert response_dict["created_at"]
 
@@ -76,7 +76,7 @@ def test_read_summary_incorrect_id(test_app_with_db):
 
 def test_read_all_summaries(test_app_with_db):
     response = test_app_with_db.post(
-        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+        "/summaries/", data=json.dumps({"url": "https://foo.bar/"})
     )
     summary_id = response.json()["id"]
 
@@ -118,7 +118,7 @@ def test_update_summary(test_app_with_db):
 
     response_dict = response.json()
     assert response_dict["id"] == summary_id
-    assert response_dict["url"] == "https://foo.bar"
+    assert response_dict["url"] == "https://foo.bar/"
     assert response_dict["summary"] == "updated!"
     assert response_dict["created_at"]
 
